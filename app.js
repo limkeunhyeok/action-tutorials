8e17ed4011a66719c9ae28b240cc48d75bd12b1d
limkeunhyeok <gorloom6425@naver.com>
2021-08-16 09:09:10 +0900

    test1

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/action", (req, res) => {
     res.send("hello world!");
 });
 
+app.get("/test1", (req, res) => {
+    res.send("test!");
+});
+
 app.listen(4000, () => {
     console.log("git action test!")
 })
